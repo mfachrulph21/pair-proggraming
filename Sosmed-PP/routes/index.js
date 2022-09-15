a5c92a3c5e6fa8bb0ccdf4911c11b5a355a96edd
fadhilhakim1 <fadhil.hakim29@gmail.com>
2022-09-16 00:04:12 +0700

    1

--- Sosmed-PP/routes/index.js
+++ Sosmed-PP/routes/index.js
@@ -14,6 +14,7 @@ router.get('/logout', UserController.goLogout)
 router.get('/home', UserController.home)
 router.get('/profile', UserController.editProfile)
 router.post('/profile', UserController.postProfile)
+router.get('/listUser', UserController.listUser)
 
 
 module.exports = router;
