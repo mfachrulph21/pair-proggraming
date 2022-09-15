66d40c4d8d6bc21b159461489f69090c32ee6f05
mfachrulph21 <mfachrulph21@gmail.com>
2022-09-16 06:47:21 +0700

    updatepagi

--- Sosmed-PP/routes/index.js
+++ Sosmed-PP/routes/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
 const router = express.Router();
-const { isLoggin, isAdmin } = require('../middlewares/auth')
+const { isLoggin, isAdmin } = require('../middlewares/auth');
+const multer = require('multer')
+
+
+const fileStorageEngine = multer.diskStorage({
+    destination : (req, file, cb) => {
+      cb(null, "../images");
+    }, 
+      filename: (req, file, cb) => {
+      cb(null, Date.now() + "--" + file.originalname)
+    }
+})
+  
+const upload = multer({ storage : fileStorageEngine})
+  
+// app.post('/singe', upload.single('image'), (req, res) => {
+//     console.log(req.file);  
+//     res.send("single file upload success")
+// })
+
 
 router.get('/register', UserController.formRegister)
 router.post('/register', UserController.postRegister)
@@ -15,6 +34,7 @@ router.get('/home', UserController.home)
 router.post('/home', UserController.handleTweet)
 router.get('/profile', UserController.editProfile)
 router.post('/profile', UserController.postProfile)
+// router.post('/profile',  upload.single("photo"), UserController.postProfile)
 router.get('/profile/detail', UserController.detailProfile)
 
 module.exports = router;
